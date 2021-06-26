25c55caae0ccd6881bf21e2a00eb84125a2a370c
cyrus-git <kurimarron238@gmail.com>
2021-06-26 17:13:00 +0900

    reset top page icon to signature

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faHandPeace } from '@fortawesome/fontawesome-free-regular';
+import { faSignature } from '@fortawesome/free-solid-svg-icons'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <FontAwesomeIcon icon={faHandPeace} transform="grow-18" />
+            <FontAwesomeIcon icon={faSignature} transform="grow-18" />
         </div>
         <div className="content">
             <div className="inner">
