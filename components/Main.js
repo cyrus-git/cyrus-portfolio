e1589abe46005ea74342a36f16c585c390607a4a
cyrus-git <kurimarron238@gmail.com>
2021-06-26 17:10:07 +0900

    create about page

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter'
-import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook'
 import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram'
 import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
 
@@ -31,8 +30,11 @@ class Main extends React.Component {
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
-          <span className="image main"><img src="/static/images/pic03.jpg" alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <span className="image main"><img src="/static/images/cyrus03.jpg" alt="" /></span>
+          <p>
+            My name is Cyrus and I am a medical student and a portrait photographer based in Tokyo, Japan. I study Medicine in a medical school in Tokyo and I'm a fifth grade student. I started learning Programming with dreaming more technological Medicine.<br/>
+            I also shoot portrait photos in <a href="https://corporate.lovegraph.me/en" target="_blank">Lovegraph Inc.</a>
+          </p>
           {close}
         </article>
 
